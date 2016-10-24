b31df7e13a35d3e45a1f9907f1bc308380b1831f
Effie Karageorgos <effie.karageorgos@qiotec.com>
2016-10-24 11:18:09 -0400

    a message

--- src/main/java/io/qio/qa/lib/ehm/common/APITestUtil.java
+++ src/main/java/io/qio/qa/lib/ehm/common/APITestUtil.java
@@ -98,6 +98,7 @@ public class APITestUtil {
 			Class[] methodArgs = new Class[5];
 			methodArgs[0] = methodArgs[1] = methodArgs[2] = methodArgs[3] = String.class;
 			methodArgs[4] = APIRequestHelper.class;
+			logger.info("ECHO22 "+methodArgs.toString());
 			Method retrieveMethod = apiHelperObj.getClass().getMethod("retrieve", methodArgs);
 
 			ConnectionResponse conRespGet = (ConnectionResponse) retrieveMethod.invoke(apiHelperObj, microservice, environment, searchBy, searchValue, apiRequestHelper);
