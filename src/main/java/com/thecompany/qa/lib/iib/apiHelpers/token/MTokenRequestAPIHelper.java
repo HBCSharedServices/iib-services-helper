bd0741448ce1b79bb8a1f97de011efc74a7dc37a
ekarageorgos <effie.karageorgos@hbc.com>
2019-09-29 19:52:23 -0400

    updates

--- src/main/java/com/thecompany/qa/lib/iib/apiHelpers/token/MTokenRequestAPIHelper.java
+++ src/main/java/com/thecompany/qa/lib/iib/apiHelpers/token/MTokenRequestAPIHelper.java
@@ -7,7 +7,6 @@ package com.thecompany.qa.lib.iib.apiHelpers.token;
 import com.thecompany.qa.lib.apiHelpers.APIRequestHelper;
 import com.thecompany.qa.lib.common.MBaseAPIHelper;
 import com.thecompany.qa.lib.connection.ConnectionResponse;
-import com.thecompany.qa.lib.assertions.CustomAssertions;
 
 public class MTokenRequestAPIHelper extends MBaseAPIHelper {
 	private final String createTokenEndpoint = "/tokenization/tokenex/create/token";
