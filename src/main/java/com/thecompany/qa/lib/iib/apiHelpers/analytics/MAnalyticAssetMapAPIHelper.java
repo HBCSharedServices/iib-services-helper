5a567b9bb45719233e867e090aa4fadbd9f9be77
ekarageorgos <effie.karageorgos@hbc.com>
2019-10-15 21:24:58 -0400

    updates

--- src/main/java/com/thecompany/qa/lib/iib/apiHelpers/analytics/MAnalyticAssetMapAPIHelper.java
+++ src/main/java/com/thecompany/qa/lib/iib/apiHelpers/analytics/MAnalyticAssetMapAPIHelper.java
@@ -9,16 +9,16 @@ import com.thecompany.qa.lib.common.MBaseAPIHelper;
 import com.thecompany.qa.lib.connection.ConnectionResponse;
 
 public class MAnalyticAssetMapAPIHelper extends MBaseAPIHelper {
-	private final String createOrUpdateUserGroupEndpoint = "/analyticassetmap";
-	private final String getOrDeleteSingleUserGroupEndpointAbstract = "/analyticassetmap/{analyticAssetMapId}";
-	private final String getAllUserGroupsEndpoint = "/analyticassetmap";
+	private final String createOrUpdateAnalyticAssetMapEndpoint = "/analyticassetmap";
+	private final String getOrDeleteSingleAnalyticAssetMapEndpoint = "/analyticassetmap/{analyticAssetMapId}";
+	private final String getAllAnalyticAssetMapsEndpoint = "/analyticassetmap";
 
-	public String getGetOrDeleteSingleUserGroupEndpointAbstract() {
-		return getOrDeleteSingleUserGroupEndpointAbstract;
+	public String getOrDeleteSingleAnalyticAssetMapEndpoint() {
+		return getOrDeleteSingleAnalyticAssetMapEndpoint;
 	}
 
 	public ConnectionResponse create(String microservice, String environment, String payload, APIRequestHelper apiRequestHeaders) {
-		return super.create(microservice, environment, createOrUpdateUserGroupEndpoint, payload, apiRequestHeaders);
+		return super.create(microservice, environment, createOrUpdateAnalyticAssetMapEndpoint, payload, apiRequestHeaders);
 	}
 
 	public void delete(String microservice, String environment, String analyticAssetMapId, APIRequestHelper apiRequestHeaders) {
@@ -30,7 +30,7 @@ public class MAnalyticAssetMapAPIHelper extends MBaseAPIHelper {
 	}
 
 	public ConnectionResponse retrieve(String microservice, String environment, APIRequestHelper apiRequestHeaders) {
-		return super.retrieve(microservice, environment, getAllUserGroupsEndpoint, apiRequestHeaders);
+		return super.retrieve(microservice, environment, getAllAnalyticAssetMapsEndpoint, apiRequestHeaders);
 	}
 
 	public ConnectionResponse retrieve(String microservice, String environment, String analyticAssetMapId, APIRequestHelper apiRequestHeaders) {
@@ -38,7 +38,7 @@ public class MAnalyticAssetMapAPIHelper extends MBaseAPIHelper {
 	}
 
 	protected String replaceAnalyticAssetMapIdIdInSingleAnalyticAssetMapEndpoint(String analyticAssetMapId) {
-		String singleAnalyticAssetMapEndpoint = getOrDeleteSingleUserGroupEndpointAbstract.replace("{analyticAssetMapId}", analyticAssetMapId);
+		String singleAnalyticAssetMapEndpoint = getOrDeleteSingleAnalyticAssetMapEndpoint.replace("{analyticAssetMapId}", analyticAssetMapId);
 		return singleAnalyticAssetMapEndpoint;
 	}
 }
